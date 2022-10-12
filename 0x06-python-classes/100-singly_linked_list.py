dc4762155276aa65b25675f46ca0d44349019554
Emasco001 <malachy820@gmail.com>
2022-10-11 20:10:01 -0700

    printing square

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -1,77 +1,56 @@
 #!/usr/bin/python3
 class Node:
-    """
-    Node for singly linked list
-    Attributes:
-        data (int): data of node
-        next_node (Node): reference node to next in linked list
-    """
     def __init__(self, data, next_node=None):
         self.data = data
         self.next_node = next_node
 
     @property
     def data(self):
-        """ data getter """
         return self.__data
 
     @data.setter
-    def data(self, data):
-        """ data setter """
-        if type(data) != int:
-            raise TypeError("data must be an integer")
-        self.__data = data
+    def data(self, value):
+        if not isinstance(value, int):
+            raise TypeError('data must be an integer')
+        self.__data = value
 
     @property
     def next_node(self):
-        """ next node getter """
         return self.__next_node
 
     @next_node.setter
-    def next_node(self, next_node):
-        """ next node setter """
-        if next_node is not None and type(next_node) != Node:
-            raise TypeError("next_node must be a Node object")
-        self.__next_node = next_node
+    def next_node(self, value):
+        if value is not None and type(value) != Node:
+            raise TypeError('next_node must be a Node object')
+        self.__next_node = value
 
 
 class SinglyLinkedList:
-    """
-    singly linked list class
-    Attributes:
-        head (Node): head node
-    """
     def __init__(self):
-        self.__head = None
+        self.head = None
 
     def __str__(self):
-        node = self.__head
-        linked_list = ""
-        while node is not None:
-            linked_list += str(node.data) + "\n"
+        ret = ""
+        node = self.head
+        while node:
+            ret += str(node.data) + "\n"
             node = node.next_node
-        linked_list = linked_list[:-1]
-        return linked_list
+        return ret[:-1]
 
     def sorted_insert(self, value):
-        """ inserted node sorted """
         new = Node(value)
-        if self.__head is None:
-            self.__head = new
+        if not self.head:
+            self.head = new
             return
-        nodes = []
-        cur = self.__head
-        while cur.next_node is not None:
-            cur = cur.next_node
-        cur.next_node = new
-        cur = self.__head
-        while cur is not None:
-            nodes.append(cur.data)
-            cur = cur.next_node
-        nodes.sort()
-        cur = self.__head
-        i = 0
-        while cur is not None:
-            cur.data = nodes[i]
-            cur = cur.next_node
-            i += 1
+
+        if value < self.head.data:
+            new.next_node = self.head
+            self.head = new
+            return
+
+        node = self.head
+        while node.next_node and node.next_node.data < value:
+            node = node.next_node
+        if node.next_node:
+            new.next_node = node.next_node
+        node.next_node = new
--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,61 +1,77 @@
 #!/usr/bin/python3
+"""Square module."""
+
+
 class Square:
-    """
-    Square class representation
-    Attributes:
-        __size (int): size of square
-        position (tuple): coordinates of a square
-    """
-    def __init__(self, size, position=(0, 0)):
+    """Defines a square."""
+
+    def __str__(self):
+        self.my_print()
+
+    def __init__(self, size=0, position=(0, 0)):
+        """Constructor.
+        Args:
+            size: Length of a side of the square.
+            position: Position of the square.
+        """
         self.size = size
         self.position = position
 
-    def area(self):
-        """ square area """
-        return self.__size ** 2
-
     @property
     def size(self):
-        """ size getter """
+        """Property for the length of a side of this square.
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
+        """
         return self.__size
 
     @size.setter
-    def size(self, size):
-        """ size setter """
-        if type(size) != int:
+    def size(self, value):
+        if not isinstance(value, int):
             raise TypeError('size must be an integer')
-        if size < 0:
+        if value < 0:
             raise ValueError('size must be >= 0')
-        self.__size = size
+        self.__size = value
 
     @property
     def position(self):
-        """ position getter """
+        """Property for the position of this square.
+        Raises:
+            TypeError: If value is not tuple of 2 positive integers.
+        """
         return self.__position
 
     @position.setter
-    def position(self, position):
-        """ position setter """
-        if (type(position) != tuple or
-                len(position) != 2 or
-                type(position[0]) != int or
-                type(position[1]) != int or
-                position[0] < 0 or
-                position[1] < 0):
+    def position(self, value):
+        if not isinstance(value, tuple) or len(value) != 2 or \
+         len([x for x in value if isinstance(x, int) and x >= 0]) != 2:
             raise TypeError('position must be a tuple of 2 positive integers')
-        self.__position = position
+        self.__position = value
+
+    def area(self):
+        """Area of this square.
+        Returns:
+            The size squared.
+        """
+        return self.__size ** 2
+
+    def my_sprint(self):
+        """Returns string representation of this square."""
+        ret = ""
+        if not self.size:
+            return "\n"
+
+        for i in range(self.position[1]):
+                ret += "\n"
+        for i in range(self.size):
+            for j in range(self.position[0]):
+                ret += " "
+            for j in range(self.size):
+                ret += "#"
+            ret += "\n"
+        return ret
 
     def my_print(self):
-        """ print square """
-        if self.__size == 0:
-            print()
-            return
-        for it in range(self.__position[1]):
-            print()
-        for i in range(self.__size):
-            for j in range(self.__size + self.__position[0]):
-                if j < self.__position[0]:
-                    print(" ", end="")
-                else:
-                    print("#", end="")
-            print()
+        """Prints this square."""
+        print(self.my_sprint(), end="")
